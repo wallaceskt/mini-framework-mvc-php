44b4b55b95c070669dbf312971394891c915a58a
Wallace Oliveira <wallacesales@gmail.com>
2021-06-14 00:54:04 -0300

    Criei a estrutura do projeto

---
+++ app/core/RouterCore.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\core;
+
+class RouterCore {
+    
+    public function __construct() {
+
+        echo "Router Core";
+
+    }
+
+}
---
+++ app/functions/functions.php
@@ -0,0 +1,10 @@
+<?php
+function dd($params = [], $die = true) {
+
+    echo "<pre>";
+    print_r($params);
+    echo "</pre>";
+
+    if ($die) die();
+
+}

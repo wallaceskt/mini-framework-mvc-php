6e0f3b91079efc8b0ef15b23ced079a317d499df
Wallace Oliveira <wallacesales@gmail.com>
2021-06-14 19:18:43 -0300

    Criei sistema de rota simples para chamar as rotas com funções

--- app/config/config.php
+++ app/config/config.php
@@ -4,4 +4,4 @@ define('BASE', '/mini-framework-mvc-php/');
 
 define('UNSET_URI_COUNT', 1);
 
-define('DEBUG_URI', true);
+define('DEBUG_URI', false);
--- app/config/router.php
+++ app/config/router.php
@@ -1,1 +1,18 @@
 <?php
+$this->get('/', function() {
+
+    echo "HOME!!!";
+
+});
+
+$this->get('/home/', function() {
+
+    echo "Estou na HOME!!!";
+
+});
+
+$this->get('/about/test', function() {
+
+    echo "Estou na ABOUT/TEST!!!";
+
+});
